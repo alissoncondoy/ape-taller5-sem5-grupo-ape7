2df7eca2cd37042798480cf892beb9288c29a3e7
Ad2r5 <ajvalverde4@utpl.edu.ec>
2023-11-14 11:55:51 -0500

    Ejercicios

--- Ejercicios Impares/src/ejercicios/impares/Ejercicio_1.java
+++ Ejercicios Impares/src/ejercicios/impares/Ejercicio_1.java
@@ -1,13 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package ejercicios.impares;
-
-/**
- *
- * @author Usuario iTC
- */
+import java.util.Scanner;
 public class Ejercicio_1 {
+    public static void main(String[] args) {
+        int edad;
+        Scanner dato=new Scanner(System.in);
+        System.out.println("Ingrese su edad");
+        edad = dato.nextInt();
+        if (edad >= 18) {
+            System.out.println("Puede votar");
+        }
+        else 
+            System.out.println("No puede votar");
+    }
     
 }
